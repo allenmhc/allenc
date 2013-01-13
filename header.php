db34d2f71be9ab2a0dc755ade5502c2779e5a986
Allen Cheung <me@allenc.com>
2013-01-12 18:38:13 -0800

    Add simple <head> and header HTML, background texture.

--- header.php
+++ header.php
@@ -11,7 +11,25 @@
   <title><?php wp_title(); ?></title>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/styles/index.css" type="text/css" media="screen" />
+  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+  <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS" href="<?php bloginfo('rss2_url'); ?>" />
+
+  <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+  <script>window.Modernizr || document.write('<script src="scripts/lib/modernizr-2.6.2.min.js">\x3C/script>')</script>
+  <script src="http://use.typekit.com/eyp5dlm.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="scripts/lib/jquery-1.8.3.min.js">\x3C/script>')</script>
+
   <?php wp_head(); ?>
 </head>
 <body>
+
+  <header id="site-header">
+    <hgroup id="site-title">
+      <h1 class="name"><?php bloginfo("name"); ?></h1>
+      <h3 class="description"><?php bloginfo("description"); ?></h3>
+    </hgroup>
+  </header>
+
   <div id="content" class="clearfix">
