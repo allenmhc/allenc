d188c5e10fcb598cc1d8a6b5b4697fcb64e1215f
Allen Cheung <me@allenc.com>
2013-01-12 16:14:09 -0800

    Compass integration and style file structure.

--- header.php
+++ header.php
@@ -9,6 +9,8 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <title><?php wp_title(); ?></title>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/styles/index.css" type="text/css" media="screen" />
   <?php wp_head(); ?>
 </head>
 <body>
