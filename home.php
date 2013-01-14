1b0ab2aeb650358c760303175db3b35c919b3a08
Allen Cheung <me@allenc.com>
2013-01-14 01:31:53 -0800

    Start styling on home page.

---
+++ home.php
@@ -0,0 +1,48 @@
+<?php
+/* Home template */
+define('WP_USE_THEMES', false);
+global $wp_query;
+?>
+
+<?php
+  get_header();
+?>
+
+<div id="home">
+  <section class="spine-left spine-spacer"></section>
+
+  <section class="spine-section">
+    <div class="spine-title">
+      <a class="section-title">
+        <div class="spine-marker"></div>
+        <h3 class="title-main">Home</h3>
+        <div class="section-title-animated">
+          <div class="title-separator"></div>
+          <h3 class="title-additional">Origin</h3>
+        </div>
+      </a>
+    </div>
+  </section>
+
+  <section class="spine-left spine-spacer"></section>
+
+  <section class="spine-section">
+    <div class="spine-title">
+      <a class="section-title">
+        <div class="spine-marker"></div>
+        <h3 class="title-main">Articles</h3>
+        <div class="section-title-animated">
+          <div class="title-separator"></div>
+          <h3 class="title-additional">Noteworthy</h3>
+        </div>
+      </a>
+    </div>
+    <div class="spine-left spine-content">
+      Stuff
+    </div>
+  </section>
+</div>
+
+<?php
+  get_footer();
+?>
