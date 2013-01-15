16914fc3ca82e7e33a68b1dc239a239a3f0dc187
Allen Cheung <me@allenc.com>
2013-01-15 00:26:58 -0800

    Add grid 960 bookmarklet config.

--- header.php
+++ header.php
@@ -24,6 +24,26 @@
   <?php wp_head(); ?>
 </head>
 <body>
+  <!-- Gridder config - remove on styling completion -->
+  <script type="text/javascript">
+  var gOverride = {
+    urlBase: 'http://gridder.andreehansson.se/releases/latest/',
+    gColor: '#EEEEEE',
+    gColumns: 16,
+    gOpacity: 0.35,
+    gWidth: 10,
+    pColor: '#C0C0C0',
+    pHeight: 15,
+    pOffset: 0,
+    pOpacity: 0.55,
+    center: true,
+    gEnabled: true,
+    pEnabled: true,
+    setupEnabled: true,
+    fixFlash: true,
+    size: 960
+  };
+  </script>
 
   <header id="site-header">
     <div class="centered-header">
