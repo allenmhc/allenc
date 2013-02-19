0a2486faad7366af7339eea405579890c77e0e9c
Allen Cheung <me@allenc.com>
2013-02-19 00:18:47 -0800

    Better titles.

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
-  <title><?php wp_title(); ?></title>
+  <title><?php wp_title("|", true, 'right'); ?><?php bloginfo('name'); ?></title>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/styles/index.css" type="text/css" media="screen" />
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/styles/zocial.css" type="text/css" media="screen" />
