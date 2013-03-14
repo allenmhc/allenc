91b83ecfbfc7f44f0d21c7408a49145305d95629
Allen Cheung <me@allenc.com>
2013-03-14 02:41:09 -0700

    Move JS include ordering.

--- footer.php
+++ footer.php
@@ -31,14 +31,15 @@
     </div>
   </footer>
 
-  <?php
-    wp_footer();
-  ?>
-
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="scripts/lib/jquery-1.8.3.min.js">\x3C/script>')</script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/1.7.2/moment.min.js"></script>
   <script>window.jQuery || document.write('<script src="scripts/lib/moment-1.7.2.min.js">\x3C/script>')</script>
+
+  <?php
+    wp_footer();
+  ?>
+
   <script src="<?php echo get_bloginfo('template_directory'); ?>/scripts/lib/spin.min.js"></script>
   <script src="<?php echo get_bloginfo('template_directory'); ?>/scripts/twitter.js"></script>
 </body>
