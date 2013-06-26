e2752a4c4ccb31c23c83a4a2c2b9877fe4bcacc8
Allen Cheung <me@allenc.com>
2013-06-26 12:07:32 -0700

    Upgrade Twitter API to 1.1.
    - Moved to OAuth-powered 1.1 search API.
    - Moved auth params to PHP
    - Removed JS-based search API call.
    - Slight style tweaks.

--- footer.php
+++ footer.php
@@ -1,11 +1,29 @@
   </div> <!-- end of main -->
 
+  <?php
+    define('TWITTER_CONSUMER_KEY', '4gqAvJ5V34lV2r4zkGNiWQ');
+    define('TWITTER_CONSUMER_SECRET', 'BxrM1xp64oi7Y4ecrm6zNkpYlV0TCkSUenNcP93n3Q');
+
+    require_once('codebird.php');
+    \Codebird\Codebird::setConsumerKey(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
+    $cb = \Codebird\Codebird::getInstance();
+  ?>
+
   <footer id="site-footer">
     <div class="centered-section">
       <div id="footer-alpha">
         <h4>@allenmhc</h4>
-          <img id="twitter-profile-pic" />
-        <div id="tweets"></div>
+        <?php $reply = $cb->users_show("screen_name=allenmhc", true); ?>
+        <img id="twitter-profile-pic" src="<?php echo $reply->profile_image_url_https; ?>"/>
+        <div id="tweets">
+          <?php $reply = $cb->search_tweets(array("q" => "from:allenmhc", "count" => 2), true); ?>
+          <?php foreach ($reply->statuses as $tweet): ?>
+          <a class="tweet" target="_blank" href="http://twitter.com/allenmhc/status/<?php echo $tweet->id_str; ?>">
+            <aside><?php echo date("l, g:ia", strtotime($tweet->created_at)); ?></aside>
+            <p><?php echo $tweet->text; ?></p>
+          </a>
+          <?php endforeach; ?>
+        </div>
       </div>
       <div id="footer-beta">
         <a id="rss" href="<?php bloginfo('rss2_url'); ?>">
@@ -33,15 +51,10 @@
 
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="scripts/lib/jquery-1.8.3.min.js">\x3C/script>')</script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/1.7.2/moment.min.js"></script>
-  <script>window.jQuery || document.write('<script src="scripts/lib/moment-1.7.2.min.js">\x3C/script>')</script>
 
   <?php
     wp_footer();
   ?>
-
-  <script src="<?php echo get_bloginfo('template_directory'); ?>/scripts/lib/spin.min.js"></script>
-  <script src="<?php echo get_bloginfo('template_directory'); ?>/scripts/twitter.js"></script>
 </body>
 
 </html>
